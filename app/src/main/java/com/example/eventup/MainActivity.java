f70a9bd4d5cb267f488b00fce47e7989fd979bd9
carloosbravo <carloosbravo04@gmail.com>
2023-10-25 13:54:37 +0200

    Firebase introduced

--- app/src/main/java/com/example/eventup/MainActivity.java
+++ app/src/main/java/com/example/eventup/MainActivity.java
@@ -1,8 +1,18 @@
 package com.example.eventup;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseFirestore firestorDb = FirebaseFirestore.getInstance();
+        Map<String, String> users = new HashMap<>();
+        users.put("email","g@g.com");
+        users.put("name","Gonza");
+
+        firestorDb.collection("EventUp").document("users")
+                .set(users)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Log.d("DEBUG","TODO OK");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d("ERROR",e.getMessage());
+                    }
+                });
+
+            };
     }
+
 }
